09a15abc59907a4befc88d9562915ec6737591b2
Dominic Zettl <dominic.zettl@iota.org>
2021-02-01 13:32:35 +0100

    initial api service

---
+++ api/.eslintrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier']
+};
