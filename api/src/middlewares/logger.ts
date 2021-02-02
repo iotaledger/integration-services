b36d7567a47ebbbf6c49a490c50d61917a495e33
Dominic Zettl <dominic.zettl@iota.org>
2021-02-02 10:30:41 +0100

    adjust logger middleware

--- api/src/middlewares/logger.ts
+++ api/src/middlewares/logger.ts
@@ -2,6 +2,6 @@ import { NextFunction, Request, Response } from 'express';
 
 // TODO
 export function loggerMiddleware(request: Request, response: Response, next: NextFunction): void {
-  console.log(`${request.method} ${request.path}`);
+  console.log(`${request.method} ${request.path} ${JSON.stringify(request.query)}`);
   next();
 }
