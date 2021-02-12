d30fca532888e2036f54ce9c1efbd033660c055a
Dominic Zettl <dominic.zettl@iota.org>
2021-02-12 09:53:07 +0100

    remove -service from prefix

--- api/src/index.ts
+++ api/src/index.ts
@@ -23,7 +23,7 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(loggerMiddleware);
 
-useRouter(app, '/channel-info-service', channelInfoRouter);
+useRouter(app, '/channel-info', channelInfoRouter);
 
 app.use(errorMiddleware);
 
