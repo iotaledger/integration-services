62efda31db87a8d72628109714695ebf3299c8a7
Dominic Zettl <dominic.zettl@iota.org>
2021-05-18 15:28:22 +0200

    add test for channel creation

---
+++ api/src/routes/channel/index.test.ts
@@ -0,0 +1,95 @@
+import { StatusCodes } from 'http-status-codes';
+import { ChannelRoutes } from '.';
+import { ChannelInfo } from '../../models/types/channel-info';
+import { AccessRights, Subscription, SubscriptionType } from '../../models/types/subscription';
+import { ChannelInfoService } from '../../services/channel-info-service';
+import { ChannelService } from '../../services/channel-service';
+import { StreamsService } from '../../services/streams-service';
+import { SubscriptionService } from '../../services/subscription-service';
+import { UserService } from '../../services/user-service';
+
+describe('test channel routes', () => {
+	//const serverSecret = 'very-secret-secret';
+	let sendMock: any, sendStatusMock: any, nextMock: any, res: any;
+	let channelService: ChannelService, channelRoutes: ChannelRoutes, streamsService: StreamsService;
+	let channelInfoService: ChannelInfoService, userService: UserService, subscriptionService: SubscriptionService;
+
+	beforeEach(() => {
+		sendMock = jest.fn();
+		sendStatusMock = jest.fn();
+		nextMock = jest.fn();
+		//	const config: any = {
+		//		serverIdentityId: ''
+		//	};
+		userService = new UserService();
+		streamsService = new StreamsService();
+		channelInfoService = new ChannelInfoService(userService);
+		subscriptionService = new SubscriptionService(streamsService, channelInfoService);
+		channelService = new ChannelService(streamsService, channelInfoService, subscriptionService);
+		channelRoutes = new ChannelRoutes(channelService);
+
+		res = {
+			send: sendMock,
+			sendStatus: sendStatusMock,
+			status: jest.fn(() => res)
+		};
+	});
+
+	describe('test create channel route', () => {
+		it('should call nextMock if no body is provided', async () => {
+			const req: any = {
+				params: {},
+				user: { userId: undefined },
+				body: undefined // no body
+			};
+
+			await channelRoutes.createChannel(req, res, nextMock);
+			expect(nextMock).toHaveBeenCalledWith(new Error("Cannot read property 'topics' of undefined"));
+		});
+
+		it('should bad request if no userId is provided', async () => {
+			const req: any = {
+				params: {},
+				user: { userId: undefined }, //no userId,
+				body: {}
+			};
+
+			await channelRoutes.createChannel(req, res, nextMock);
+			expect(res.sendStatus).toHaveBeenCalledWith(StatusCodes.BAD_REQUEST);
+		});
+		it('should create and return a channel for the user', async () => {
+			const req: any = {
+				params: {},
+				user: { userId: 'did:iota:1234' },
+				body: { topics: [], seed: 'verysecretseed' }
+			};
+
+			const expectedSubscription: Subscription = {
+				accessRights: AccessRights.ReadAndWrite,
+				channelAddress: '1234234234',
+				isAuthorized: true,
+				seed: 'verysecretseed',
+				state: 'uint8array string of subscription state',
+				subscriptionLink: '1234234234',
+				type: SubscriptionType.Author,
+				userId: 'did:iota:1234'
+			};
+			const expectedChannelInfo: ChannelInfo = { authorId: 'did:iota:1234', channelAddress: '1234234234', latestLink: '1234234234', topics: [] };
+
+			const exportSubscriptionSpy = spyOn(streamsService, 'exportSubscription').and.returnValue('uint8array string of subscription state');
+			const createSpy = spyOn(streamsService, 'create').and.returnValue({ seed: 'verysecretseed', author: {}, channelAddress: '1234234234' });
+			const addSubscriptionSpy = spyOn(subscriptionService, 'addSubscription');
+			const addChannelInfoSpy = spyOn(channelInfoService, 'addChannelInfo');
+
+			await channelRoutes.createChannel(req, res, nextMock);
+
+			expect(createSpy).toHaveBeenCalledWith('verysecretseed');
+			expect(exportSubscriptionSpy).toHaveBeenCalledWith({}, 'test123');
+			expect(addSubscriptionSpy).toHaveBeenCalledWith(expectedSubscription);
+			expect(addChannelInfoSpy).toHaveBeenCalledWith(expectedChannelInfo);
+			expect(res.status).toHaveBeenCalledWith(StatusCodes.CREATED);
+			// TODO author should be the exported string not object
+			expect(res.send).toHaveBeenCalledWith({ author: {}, channelAddress: '1234234234', seed: 'verysecretseed' });
+		});
+	});
+});
