97def6a991093bcb61ba16e2d7b42b87ad269d7d
Dominic Zettl <dominic.zettl@iota.org>
2021-05-21 10:53:08 +0200

    fix further tests

--- api/src/pools/subscription-pools/index.test.ts
+++ api/src/pools/subscription-pools/index.test.ts
@@ -2,7 +2,7 @@ import { SubscriptionPool } from '.';
 import * as SubscriptionDb from '../../database/subscription';
 
 describe('test subscription pool', () => {
-	const pool = SubscriptionPool.getInstance();
+	const pool = SubscriptionPool.getInstance('node');
 	beforeEach(() => {
 		pool.add({ id: 'imanauthor1' } as any, 'iota:did:imanauthor1', 'test123', true);
 		pool.add({ id: 'imanauthor2' } as any, 'iota:did:imanauthor2', 'test123', true);
@@ -12,13 +12,14 @@ describe('test subscription pool', () => {
 
 	describe('check if expected subscriptions will be returned', () => {
 		it('should return expected subscriptions', async () => {
+			const pw = 'test123';
 			const getSubscriptionSpy = spyOn(SubscriptionDb, 'getSubscription').and.returnValue(null);
-			expect(await pool.get('test123', 'iota:did:imanauthor1', true)).toEqual({ id: 'imanauthor1' });
-			expect(await pool.get('test123', 'iota:did:imanauthor2', true)).toEqual({ id: 'imanauthor2' });
-			expect(await pool.get('test123', 'iota:did:imanauthor2', false)).toEqual(undefined);
-			expect(await pool.get('test123', 'iota:did:imasubscriber1', false)).toEqual({ id: 'imasubscriber1' });
-			expect(await pool.get('test123', 'iota:did:imasubscriber2', false)).toEqual({ id: 'imasubscriber2' });
-			expect(await pool.get('test123', 'iota:did:imasubscriber2', true)).toEqual(undefined);
+			expect(await pool.get('test123', 'iota:did:imanauthor1', true, pw)).toEqual({ id: 'imanauthor1' });
+			expect(await pool.get('test123', 'iota:did:imanauthor2', true, pw)).toEqual({ id: 'imanauthor2' });
+			expect(await pool.get('test123', 'iota:did:imanauthor2', false, pw)).toEqual(undefined);
+			expect(await pool.get('test123', 'iota:did:imasubscriber1', false, pw)).toEqual({ id: 'imasubscriber1' });
+			expect(await pool.get('test123', 'iota:did:imasubscriber2', false, pw)).toEqual({ id: 'imasubscriber2' });
+			expect(await pool.get('test123', 'iota:did:imasubscriber2', true, pw)).toEqual(undefined);
 			expect(getSubscriptionSpy).toHaveBeenCalledTimes(2);
 		});
 	});
