1e526bfdac73ef7f5f28ada4f7119fb9bfd856db
Dominic Zettl <dominic.zettl@iota.org>
2021-05-21 15:52:07 +0200

    add api key middleware

--- api/src/models/config/index.ts
+++ api/src/models/config/index.ts
@@ -8,6 +8,7 @@ export interface Config {
 	serverIdentityId: string;
 	identityConfig: IdentityConfig;
 	serverSecret: string;
+	apiKey: string | undefined;
 	streamsNode: string;
 	jwtExpiration: string;
 }
