a7e26f8ace96efbcf10754454e97599132e081d3
Dominic Zettl <dominic.zettl@iota.org>
2021-05-26 17:47:29 +0200

    update logs on client

--- clients/src/authenticate/index.ts
+++ clients/src/authenticate/index.ts
@@ -3,23 +3,23 @@ import { getHexEncodedKey, signNonce } from '../utils/encryption';
 import axios from 'axios';
 
 export const fetchAuth = async (identity: any) => {
-	console.log('requesting challenge to sign...');
+	console.log('requesting nonce to sign...');
 
 	const url = `${Config.baseUrl}/api/v1/authentication/prove-ownership/${identity.doc.id}`;
 	const res = await axios.get(url);
 	if (res.status !== 200) {
-		console.error('didnt receive status 200 on get-challenge!');
+		console.error('didnt receive status 200 on get request for prove-ownership!');
 		return;
 	}
 	const body = await res.data;
 	const nonce: string = body.nonce;
-	console.log('received challenge: ', nonce);
+	console.log('received nonce: ', nonce);
 
 	const encodedKey = await getHexEncodedKey(identity.key.secret);
 	const signedNonce = await signNonce(encodedKey, nonce);
-	console.log('signed challenge: ', signedNonce);
+	console.log('signed nonce: ', signedNonce);
 
-	console.log('requesting authentication token using signed challenge...', identity.doc.id);
+	console.log('requesting authentication token using signed nonce...', identity.doc.id);
 	const response = await axios.post(`${Config.baseUrl}/api/v1/authentication/prove-ownership/${identity.doc.id}`, JSON.stringify({ signedNonce }), {
 		method: 'post',
 		headers: { 'Content-Type': 'application/json' }
