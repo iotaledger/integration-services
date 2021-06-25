7e8c305de1f77e3d05aa5e940237555c1414337d
Tim <tim.sigl@hotmail.de>
2021-06-25 09:39:12 +0200

    added swaggerjsdoc and swagger ui express

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,17 +1,22 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
 dotenv.config();
+import swaggerUi from "swagger-ui-express";
 import { errorMiddleware } from './middlewares/error';
 import { authenticationRouter, verificationRouter, channelInfoRouter, channelRouter, subscriptionRouter, identityRouter } from './routes/router';
 import { MongoDbService } from './services/mongodb-service';
 import { CONFIG } from './config';
 import morgan from 'morgan';
 import { setupApi } from './setup';
+import swaggerJsdoc from 'swagger-jsdoc';
+import './routes/authentication/index'
+import './routes/authentication/usersController'
 
 function useRouter(app: express.Express, prefix: string, router: express.Router) {
 	console.log(router.stack.map((r) => `${Object.keys(r?.route?.methods)?.[0].toUpperCase()}  ${prefix}${r?.route?.path}`));
 	app.use(prefix, router);
 }
+
 
 async function startServer() {
 	// setup did for server if not exists
@@ -24,9 +29,29 @@ async function startServer() {
 	const version = CONFIG.apiVersion;
 	const loggerMiddleware = morgan('combined');
 
+	const options = {
+		definition: {
+		  openapi: '3.0.0',
+		  info: {
+			title: 'Hello World',
+			version: '1.0.0',
+		  },
+		  servers: [
+			  {
+				"url": `/api/${version}`,
+				"description": "Local Dev" 
+			  }
+		  ]
+		},
+		apis: ['./src/routes/**/*.ts'], // files containing annotations as above
+	  };
+	
+	  const openapiSpecification = swaggerJsdoc(options);
+
 	app.use(express.json({ limit: '10mb' }));
 	app.use(express.urlencoded({ limit: '10mb', extended: true }));
 	app.use(loggerMiddleware);
+	app.use("/docs", swaggerUi.serve, swaggerUi.setup(openapiSpecification));
 
 	const prefix = `/api/${version}`;
 	useRouter(app, prefix + '/channel-info', channelInfoRouter);
@@ -35,6 +60,7 @@ async function startServer() {
 	useRouter(app, prefix + '/identities', identityRouter);
 	useRouter(app, prefix + '/authentication', authenticationRouter);
 	useRouter(app, prefix + '/verification', verificationRouter);
+
 
 	app.use(errorMiddleware);
 	app.listen(port, async () => {
