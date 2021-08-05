955b8275337ef27f37a55a781a6defc7120830e2
Dominic Zettl <dominic.zettl@iota.org>
2021-08-05 17:26:11 +0200

    add description for channel payloads

--- api/src/models/schemas/request-response-body/subscription-bodies.ts
+++ api/src/models/schemas/request-response-body/subscription-bodies.ts
@@ -7,8 +7,14 @@ export const AuthorizeSubscriptionBodySchema = Type.Object({
 });
 
 export const RequestSubscriptionBodySchema = Type.Object({
-	subscriptionPassword: Type.Optional(Type.String({ minLength: 8 })), // TODO#156 use to decrypt/encrypt data and state
-	seed: Type.Optional(Type.Union([Type.String({ minLength: 1 }), Type.Null()])),
+	subscriptionPassword: Type.Optional(
+		Type.String({
+			minLength: 8,
+			description:
+				'If a subscriptionPassword is set, all data is encrypted with the password. It need to be made sure, the subscription password is sent when interacting with the APIs of the channel-service and subscription-service.'
+		})
+	), // TODO#156 use to decrypt/encrypt data and state
+	seed: Type.Optional(Type.Union([Type.String({ minLength: 1, description: 'If left empty the api will generate a seed.' }), Type.Null()])),
 	accessRights: Type.Optional(Type.Union([Type.Enum(AccessRights), Type.Null()])),
 	presharedKey: Type.Optional(Type.String({ maxLength: 16, minLength: 16 }))
 });
