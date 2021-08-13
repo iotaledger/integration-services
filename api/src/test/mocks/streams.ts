c2c101a24555ab2c35db5b87d03e0489ed4aa335
Dominic Zettl <dominic.zettl@iota.org>
2021-08-13 11:04:00 +0200

    return keyloadlink when creating a channel in tests

--- api/src/test/mocks/streams.ts
+++ api/src/test/mocks/streams.ts
@@ -1,1 +1,5 @@
-export const AuthorMock = { clone: () => AuthorMock, sync_state: jest.fn(), get_public_key: () => 'test-author-public-key' };
+export const AuthorMock = {
+	clone: () => AuthorMock,
+	sync_state: jest.fn(),
+	get_public_key: () => 'test-author-public-key'
+};
