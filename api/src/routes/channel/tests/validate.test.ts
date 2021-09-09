a2c5646fc88b08077d7fdc81bba9eea9d2f34ea3
Dominic Zettl <dominic.zettl@iota.org>
2021-09-09 14:21:14 +0200

    add functional tests for validate route

---
+++ api/src/routes/channel/tests/validate.test.ts
@@ -0,0 +1,211 @@
+import { StatusCodes } from 'http-status-codes';
+import { ChannelRoutes } from '..';
+import { AccessRights, SubscriptionType } from '../../../models/schemas/subscription';
+import { SubscriptionPool } from '../../../pools/subscription-pools';
+import { ChannelInfoService } from '../../../services/channel-info-service';
+import { ChannelService } from '../../../services/channel-service';
+import { StreamsService } from '../../../services/streams-service';
+import { SubscriptionService } from '../../../services/subscription-service';
+import { UserService } from '../../../services/user-service';
+import { StreamsConfigMock } from '../../../test/mocks/config';
+import { TestUsersMock } from '../../../test/mocks/identities';
+import { LoggerMock } from '../../../test/mocks/logger';
+import { AuthorMock } from '../../../test/mocks/streams';
+
+describe('test validate route', () => {
+	let sendMock: any, sendStatusMock: any, nextMock: any, res: any;
+	let channelService: ChannelService, channelRoutes: ChannelRoutes, streamsService: StreamsService, subscriptionPool: SubscriptionPool;
+	let channelInfoService: ChannelInfoService, userService: UserService, subscriptionService: SubscriptionService;
+
+	const logs = [
+		{
+			link: 'c477ad1063fb6543522fc97026e813387e5ad939dfa3a2d413a6b881b338c7910000000000000000:c2bf8e1ccc98cb7b5ba286c4',
+			channelLog: {
+				created: '2021-09-06T14:30:08+02:00',
+				type: 'author-test',
+				metadata: {
+					somedata: 22
+				},
+				publicPayload: {
+					data: 'testdata123'
+				},
+				payload: 'I am a string from the author'
+			},
+			messageId: '4567570d36e11dc377faee93c8c182b0b44bcd816b4fd0e48ec85a8e8a49f69d',
+			imported: '2021-09-06T14:30:14+02:00'
+		},
+		{
+			link: 'c477ad1063fb6543522fc97026e813387e5ad939dfa3a2d413a6b881b338c7910000000000000000:9a52fdce3e7ee09dcce3a6e1',
+			channelLog: {
+				created: '2021-09-06T14:30:05+02:00',
+				type: 'author-test',
+				metadata: {
+					somedata: 22
+				},
+				publicPayload: {
+					data: 'testdata123'
+				},
+				payload: 'I am a string from the author'
+			},
+			messageId: '281ff5b9eea55c8eed82bef64b83f0dfe7bc175677807266d28fd900a60ac26b',
+			imported: '2021-09-06T14:30:07+02:00'
+		},
+		{
+			link: 'c477ad1063fb6543522fc97026e813387e5ad939dfa3a2d413a6b881b338c7910000000000000000:55c9c94f5a485a8911b3afb9',
+			channelLog: {
+				created: '2021-09-06T14:27:54+02:00',
+				type: 'author-test',
+				metadata: {
+					somedata: 22
+				},
+				publicPayload: {
+					publicData: 'testdata123'
+				},
+				payload: 'I am a string from the author'
+			},
+			messageId: '1a6bca1cae3b7f043fbce1eeb4a95b82ba70af4db2c1cdc8f149f55f79ca084b',
+			imported: '2021-09-06T14:27:58+02:00'
+		}
+	];
+
+	beforeEach(() => {
+		sendMock = jest.fn();
+		sendStatusMock = jest.fn();
+		nextMock = jest.fn();
+		const config = StreamsConfigMock;
+		userService = new UserService({} as any, '', LoggerMock);
+		streamsService = new StreamsService(config, LoggerMock);
+		channelInfoService = new ChannelInfoService(userService);
+		subscriptionPool = new SubscriptionPool(streamsService, 20);
+		subscriptionService = new SubscriptionService(streamsService, channelInfoService, subscriptionPool, config);
+		channelService = new ChannelService(streamsService, channelInfoService, subscriptionService, subscriptionPool, config);
+		channelRoutes = new ChannelRoutes(channelService, LoggerMock);
+
+		res = {
+			send: sendMock,
+			sendStatus: sendStatusMock,
+			status: jest.fn(() => res)
+		};
+	});
+
+	it('should return bad request if no channelAddress is provided', async () => {
+		const req: any = {
+			params: {},
+			user: TestUsersMock[0],
+			body: {}
+		};
+
+		await channelRoutes.validateLogs(req, res, nextMock);
+		expect(res.status).toHaveBeenCalledWith(StatusCodes.BAD_REQUEST);
+		expect(res.send).toHaveBeenCalledWith({ error: 'no channelAddress or identityId provided' });
+	});
+
+	it('should return bad request if no channelLogs are provided', async () => {
+		const req: any = {
+			params: { channelAddress: '123456' },
+			user: TestUsersMock[0],
+			body: [] // empty channelLogs
+		};
+
+		await channelRoutes.validateLogs(req, res, nextMock);
+		expect(res.status).toHaveBeenCalledWith(StatusCodes.BAD_REQUEST);
+		expect(res.send).toHaveBeenCalledWith({ error: 'no logs provided' });
+	});
+
+	it('should throw an error since no subscription was found', async () => {
+		const channelAddress = '123456';
+		const user = TestUsersMock[0];
+		const req: any = {
+			params: { channelAddress },
+			user,
+			body: logs
+		};
+		const loggerSpy = spyOn(LoggerMock, 'error');
+		const getSubscriptionSpy = spyOn(subscriptionService, 'getSubscription').and.returnValue(null); // no subscription found
+
+		await channelRoutes.validateLogs(req, res, nextMock);
+
+		expect(getSubscriptionSpy).toHaveBeenCalledWith(channelAddress, user.identityId);
+		expect(loggerSpy).toHaveBeenCalledWith(new Error('no subscription found!'));
+		expect(nextMock).toHaveBeenCalledWith(new Error('could not validate the channel data'));
+	});
+
+	it('should throw an error since subscription has no read right', async () => {
+		const channelAddress = '123456';
+		const user = TestUsersMock[0];
+		const req: any = {
+			params: { channelAddress },
+			user,
+			body: logs
+		};
+		const loggerSpy = spyOn(LoggerMock, 'error');
+		const getSubscriptionSpy = spyOn(subscriptionService, 'getSubscription').and.returnValue({
+			keyloadLink: 'testlink',
+			publicKey: 'testkey',
+			accessRights: AccessRights.Write // wrong access rights
+		});
+
+		await channelRoutes.validateLogs(req, res, nextMock);
+
+		expect(getSubscriptionSpy).toHaveBeenCalledWith(channelAddress, user.identityId);
+		expect(loggerSpy).toHaveBeenCalledWith(new Error('not allowed to validate the logs from the channel'));
+		expect(nextMock).toHaveBeenCalledWith(new Error('could not validate the channel data'));
+	});
+
+	it('should throw an error since no streams sub is found', async () => {
+		const channelAddress = '123456';
+		const user = TestUsersMock[0];
+		const req: any = {
+			params: { channelAddress },
+			user,
+			body: logs
+		};
+		const getSubscriptionSpy = spyOn(subscriptionService, 'getSubscription').and.returnValue({
+			keyloadLink: 'testlink',
+			publicKey: 'testkey',
+			accessRights: AccessRights.Read,
+			type: SubscriptionType.Author
+		});
+		const getSubSpy = spyOn(subscriptionPool, 'get').and.returnValue(null); // no subscriber found
+		const loggerSpy = spyOn(LoggerMock, 'error');
+
+		await channelRoutes.validateLogs(req, res, nextMock);
+
+		expect(getSubscriptionSpy).toHaveBeenCalledWith(channelAddress, user.identityId);
+		expect(getSubSpy).toHaveBeenCalledWith(channelAddress, user.identityId, true);
+		expect(loggerSpy).toHaveBeenCalledWith(
+			new Error('no author/subscriber found with channelAddress: 123456 and identityId: did:iota:6cTkp3gCV3yifiGDHUK4x1omXb6yFBTRg7NS2x3kBDUm')
+		);
+		expect(nextMock).toHaveBeenCalledWith(new Error('could not validate the channel data'));
+	});
+
+	it('should validate the logs', async () => {
+		const channelAddress = '123456';
+		const user = TestUsersMock[0];
+		const req: any = {
+			params: { channelAddress },
+			user,
+			body: logs
+		};
+		const getSubscriptionSpy = spyOn(subscriptionService, 'getSubscription').and.returnValue({
+			keyloadLink: 'testlink',
+			publicKey: 'testkey',
+			accessRights: AccessRights.Read,
+			type: SubscriptionType.Author
+		});
+		const getSubSpy = spyOn(subscriptionPool, 'get').and.returnValue(AuthorMock);
+
+		await channelRoutes.validateLogs(req, res, nextMock);
+
+		const expectedValidatedLogs = [
+			{ isValid: true, link: 'c477ad1063fb6543522fc97026e813387e5ad939dfa3a2d413a6b881b338c7910000000000000000:c2bf8e1ccc98cb7b5ba286c4' },
+			{ isValid: true, link: 'c477ad1063fb6543522fc97026e813387e5ad939dfa3a2d413a6b881b338c7910000000000000000:9a52fdce3e7ee09dcce3a6e1' },
+			{ isValid: true, link: 'c477ad1063fb6543522fc97026e813387e5ad939dfa3a2d413a6b881b338c7910000000000000000:55c9c94f5a485a8911b3afb9' }
+		];
+
+		expect(getSubscriptionSpy).toHaveBeenCalledWith(channelAddress, user.identityId);
+		expect(getSubSpy).toHaveBeenCalledWith(channelAddress, user.identityId, true);
+		expect(res.status).toHaveBeenCalledWith(StatusCodes.OK);
+		expect(res.send).toHaveBeenCalledWith(expectedValidatedLogs);
+	});
+});
