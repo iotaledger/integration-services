63bab83502d3323a7a088ba3f4c7456cec2bf7c0
Tim <tim.sigl@hotmail.de>
2021-11-15 16:03:37 +0100

    setup initial wiki structure

--- documentation/sidebars.js
+++ documentation/sidebars.js
@@ -10,10 +10,67 @@
  */
 
 module.exports = {
-  mySidebar: [{
+  docs: [
+    {
       type: 'doc',
       id: 'welcome',
       label: 'Welcome'
     },
+    {
+      type: 'category',
+      label: 'Getting Started',
+      collapsed: false,
+      items: ['getting_started/requirements', 'getting_started/configuration']
+    },
+    {
+      type: 'category',
+      label: 'Installation',
+      collapsed: true,
+      items: [
+        'installation/overview',
+        'installation/docker',
+        'installation/docker_compose',
+        'installation/kubernetes',
+        'installation/helm',
+        'installation/port_forward'
+      ]
+    },
+    {
+      type: 'category',
+      label: 'Basics',
+      collapsed: true,
+      items: ['basics/overview', 'basics/identity', 'basics/streams']
+    },
+    {
+      type: 'category',
+      label: 'Examples',
+      collapsed: true,
+      items: ['examples/create_identity', 'examples/verify_identity', 'examples/create_stream', 'examples/read_stream']
+    },
+    {
+      type: 'doc',
+      id: 'api_reference',
+      label: 'API Reference'
+    },
+    {
+      type: 'doc',
+      id: 'troubleshooting',
+      label: 'Troubleshooting'
+    },
+    {
+      type: 'doc',
+      id: 'faq',
+      label: 'FAQ'
+    },
+    {
+      type: 'doc',
+      id: 'contribute',
+      label: 'Contribute'
+    },
+    {
+      type: 'doc',
+      id: 'code_of_conduct',
+      label: 'Code of Conduct'
+    }
   ]
 };
