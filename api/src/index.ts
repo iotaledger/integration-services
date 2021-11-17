bd6019e6149c0af54c84a0e4732ed140635048bd
Dominic Zettl <dominic.zettl@iota.org>
2021-11-17 11:29:58 +0100

    remove logged bearer token

--- api/src/index.ts
+++ api/src/index.ts
@@ -64,7 +64,7 @@ async function startServer(config: Config) {
 
 	app.use(express.json({ limit: '10mb' }));
 	app.use(express.urlencoded({ limit: '10mb', extended: true }));
-	app.use(expressWinston.logger(Logger.getInstance().options));
+	app.use(expressWinston.logger(Logger.getInstance().getExpressWinstonOptions()));
 
 	app.use('/docs', swaggerUi.serve, swaggerUi.setup(openapiSpecification, { explorer: true }));
 
