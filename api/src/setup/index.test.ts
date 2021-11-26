d45332d386fa8bd8eac7daee0a3a7d0766e09508
Dominic Zettl <dominic.zettl@iota.org>
2021-11-26 11:36:34 +0100

    add logger as parameter to keygenerator

--- api/src/setup/index.test.ts
+++ api/src/setup/index.test.ts
@@ -1,23 +1,28 @@
-'use strict';
+import { KeyGenerator } from './index';
+import { ConfigurationServiceMock } from '../test/mocks/service-mocks';
+import { LoggerMock } from '../test/mocks/logger';
+('use strict');
 
 jest.mock('fs');
 
 describe('test keygen', () => {
-
+	let keyGenerator: KeyGenerator;
+	beforeEach(() => {
+		keyGenerator = new KeyGenerator(ConfigurationServiceMock, LoggerMock);
+	});
 	it('SERVER_IDENTITY file must exist', () => {
-		expect(true).toEqual(true);
-	})
+		expect(keyGenerator).not.toEqual(true);
+	});
 
 	it('SERVER_IDENTITY file must be wellformed', () => {
 		expect(true).toEqual(true);
-	})
+	});
 
 	it('if SERVER_IDENTITY exists keygeneration should do nothing', () => {
 		expect(true).toEqual(true);
-	})
+	});
 
 	it('if SERVER_IDENTITY not exists a valid key must be generated', () => {
 		expect(true).toEqual(true);
-	})
-
-})
+	});
+});
