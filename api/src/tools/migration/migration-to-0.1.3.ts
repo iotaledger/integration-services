477cad2d538206b15e737f97345251f0e806e84a
Dominic Zettl <dominic.zettl@iota.org>
2021-12-06 17:30:26 +0100

    add initial migration script for identity-docs

--- api/src/tools/migration/migration-to-0.1.3.ts
+++ api/src/tools/migration/migration-to-0.1.3.ts
@@ -37,6 +37,29 @@ const migrate = async () => {
 		[{ $set: { id: '$identityId' } }, { $unset: 'identityId' }]
 	);
 
+	// since only keys are stored this collection can be renamed!
+	// TODO remove _copy
+	const collections = await db.listCollections().toArray();
+	const collectionNames = collections.map((c) => c.name);
+	if (collectionNames.some((n) => n === 'identity-docs_copy') && !collectionNames.some((n) => n === 'identity-keys')) {
+		await db.collection('identity-docs_copy').rename('identity-keys');
+		await db.collection('identity-keys').updateMany(
+			{
+				doc: { $exists: true }
+			},
+			[
+				{
+					$set: {
+						id: '$doc.id'
+					}
+				},
+				{
+					$unset: 'doc'
+				}
+			]
+		);
+	}
+
 	await MongoDbService.disconnect();
 	console.log('migration done!');
 };
