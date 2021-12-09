60dede80980f9b70dc6b9cc5a31c1403c077e144
Dominic Zettl <dominic.zettl@iota.org>
2021-12-09 16:50:38 +0100

    adjust middleware

--- api/src/middlewares/concurrency-lock.ts
+++ api/src/middlewares/concurrency-lock.ts
@@ -1,32 +1,38 @@
 import { NextFunction, Response } from 'express';
-import { AuthenticatedRequest, LockedRequest } from '../models/types/verification';
+import { AuthenticatedRequest } from '../models/types/verification';
 import { getLock, insertLock, removeLock } from '../database/concurrency-lock';
 import { StatusCodes } from 'http-status-codes';
 import { Logger } from '../utils/logger/index';
 
 export const concurrencyLock = (lockName: string) => async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
+	const user = req?.user;
 	try {
-		const user = req.user;
 		if (!user || !user.id) {
 			return res.status(StatusCodes.UNAUTHORIZED).send({ error: 'no user id provided!' });
 		}
 
 		const lock = await getLock(user.id, lockName);
+
 		if (lock) {
 			// TODO check if lock is older than X seconds if so release it and continue
+			// ==> will be solved by TTL of MongoDB to 60s
+
 			return res.status(StatusCodes.LOCKED).send({ error: 'resource already in use, try it again!' });
 		}
 
 		await insertLock(user.id, lockName);
-
-		(req as LockedRequest).releaseLock = releaseConcurrencyLock(user.id, lockName);
 	} catch (e) {
+		await releaseConcurrencyLock(user.id, lockName);
 		return res.status(StatusCodes.INTERNAL_SERVER_ERROR);
 	}
+
 	next();
+
+	// release lock after wrapped function call has completed
+	await releaseConcurrencyLock(user.id, lockName);
 };
 
-export const releaseConcurrencyLock = (id: string, lockName: string) => async () => {
+const releaseConcurrencyLock = async (id: string, lockName: string) => {
 	const response = await removeLock(id, lockName);
 	if (response.result.n === 0) {
 		Logger.getInstance().error('could not release the lock!');
