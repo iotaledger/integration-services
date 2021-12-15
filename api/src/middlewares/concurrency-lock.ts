d7596db3627bee597fa2471650a3aab0e9220e88
Dominic Zettl <dominic.zettl@iota.org>
2021-12-15 12:04:09 +0100

    release lock in finally block

--- api/src/middlewares/concurrency-lock.ts
+++ api/src/middlewares/concurrency-lock.ts
@@ -14,25 +14,20 @@ export const concurrencyLock = (lockName: string) => async (req: AuthenticatedRe
 		const lock = await getLock(user.id, lockName);
 
 		if (lock) {
-			// TODO check if lock is older than X seconds if so release it and continue
-			// ==> will be solved by TTL of MongoDB to 60s
-
 			return res.status(StatusCodes.LOCKED).send({ error: 'resource already in use, try it again!' });
 		}
 
 		await insertLock(user.id, lockName);
+		next();
 	} catch (e) {
+		return res.sendStatus(StatusCodes.INTERNAL_SERVER_ERROR);
+	} finally {
+		// release lock after wrapped function call has completed
 		await releaseConcurrencyLock(user.id, lockName);
-		return res.status(StatusCodes.INTERNAL_SERVER_ERROR);
 	}
-
-	next();
-
-	// release lock after wrapped function call has completed
-	await releaseConcurrencyLock(user.id, lockName);
 };
 
-const releaseConcurrencyLock = async (id: string, lockName: string) => {
+export const releaseConcurrencyLock = async (id: string, lockName: string) => {
 	const response = await removeLock(id, lockName);
 	if (response.result.n === 0) {
 		Logger.getInstance().error('could not release the lock!');
