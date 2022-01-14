7168a43b6983aa5648092196f95ec464c4a0bdba
Dominic Zettl <dominic.zettl@iota.org>
2022-01-14 14:09:16 +0100

    refactor code

--- api/src/middlewares/mongodb-sanitizer.ts
+++ api/src/middlewares/mongodb-sanitizer.ts
@@ -2,29 +2,36 @@ import { NextFunction, Request, Response } from 'express';
 import * as _ from 'lodash';
 import { StatusCodes } from 'http-status-codes';
 
+const BAD_CHAR = '$';
+
 export const mongodbSanitizer = (req: Request, res: Response, next: NextFunction) => {
 	const body = req.body;
 
-	const val = hasBadCharacter(body);
+	const val = hasBadCharacter(body, BAD_CHAR);
 
 	if (val) {
-		return res.status(StatusCodes.BAD_REQUEST).send({ error: '$ is not allowed as key' });
+		return res.status(StatusCodes.BAD_REQUEST).send({ error: `${BAD_CHAR} is not allowed as key` });
 	}
 
 	next();
 };
 
-export const hasBadCharacter = (data: any): boolean => {
+const hasBadCharacter = (data: any, character: string): boolean => {
 	if (_.isObject(data)) {
 		const keys = Object.keys(data);
-		const foundBadChar = keys.map((k) => k.startsWith('$')).some((s) => s);
+		const foundBadChar = keys.map((k) => k.startsWith(character)).some((s) => s);
+
 		if (foundBadChar) {
 			return true;
 		}
+
 		const values = Object.values(data);
-		return values.map((val) => hasBadCharacter(val)).some((v) => v);
+		return recursiveCall(values, character);
 	} else if (_.isArray(data)) {
-		return data.map((obj) => hasBadCharacter(obj)).some((a) => a);
+		return recursiveCall(data, character);
 	}
+
 	return false;
 };
+
+const recursiveCall = (values: any, char: string): boolean => values.map((val: any) => hasBadCharacter(val, char)).some((v: boolean) => v);
