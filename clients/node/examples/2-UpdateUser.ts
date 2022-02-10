e8b2a48628b44b7c4e6595a55b2341119f67baaa
Dominic Zettl <dominic.zettl@iota.org>
2022-02-10 13:39:08 +0100

    fix examples 0-3

--- clients/node/examples/2-UpdateUser.ts
+++ clients/node/examples/2-UpdateUser.ts
@@ -1,38 +1,46 @@
 import { searchCriteria, IdentityClient, IdentityJson } from 'iota-is-sdk';
 import { defaultConfig } from './configuration';
 import { readFileSync } from 'fs';
-
+import _ from 'lodash';
 async function searchIdentityAndUpdate() {
-
   const identity = new IdentityClient(defaultConfig);
 
   // Recover the admin identity
-  const adminIdentity = JSON.parse(readFileSync("./adminIdentity.json").toString()) as IdentityJson;
+  const adminIdentity = JSON.parse(readFileSync('./adminIdentity.json').toString()) as IdentityJson;
 
   // Authenticate as the admin identity
   await identity.authenticate(adminIdentity.doc.id, adminIdentity.key.secret);
 
   // Search for identities with username 'User' in it
-  const search: searchCriteria = { username: 'User' };
+  const search: searchCriteria = { username: 'User-' };
 
   const identities = await identity.search(search);
 
-  console.log("Found the following identities:");
+  console.log('Found the following identities:');
   console.log(JSON.stringify(identities, null, 2));
 
   if (identities && identities[0]) {
-    // Take the first identities of the searched identities
-    const userIdentity = identities[0];
+    // Take the latest identity of the searched identities
+    const ids = identities.sort((a, b) => {
+      // @ts-ignore
+      return new Date(a.registrationDate) - new Date(b.registrationDate);
+    });
+
+    const userIdentity = ids[ids.length - 1];
+
+    console.log('userIdentiy', userIdentity);
+
+    const newUsername = 'New' + userIdentity.username;
+
     // Update the claim of the identity with a new username
     await identity.update({
       ...userIdentity,
-      username: 'NewUser'
+      username: newUsername
     });
-    console.log(`Successfully updated identity with id: ${userIdentity.id}`);
+    console.log(`Successfully updated identity with id: ${userIdentity?.id}`);
   } else {
     console.log('Could not find identities with given search criteria.');
   }
 }
 
 searchIdentityAndUpdate();
-
