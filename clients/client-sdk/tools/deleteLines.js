0746e30f137337db36899b1f18c1962136ea81be
Tim Sigl <tim.sigl@hotmail.de>
2022-03-22 09:03:16 +0100

    added api reference, license

---
+++ clients/client-sdk/tools/deleteLines.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+
+// Reads modules.md file and cuts of the first few lines till '### Type aliases'
+var data = fs.readFileSync('./temp-docs/modules.md', {encoding:'utf8'});
+data = data.split('### Type aliases')[1]
+fs.writeFileSync('./temp-docs/Types.md', data)
