c6dce1d40d632ae19e124e79b0257e742e0b9b4c
Dominic Zettl <dominic.zettl@iota.org>
2022-04-25 13:27:11 +0200

    initially instantiate the components

--- dashboard/src/routes/index.svelte
+++ dashboard/src/routes/index.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script context="module" lang="ts">
+	export const prerender = true;
+</script>
+
+<script lang="ts">
+	import { goto } from '$app/navigation';
+
+	import { LoginRegisterManager } from '@iota/is-ui-components';
+	import { Col, Container, Row } from 'sveltestrap';
+</script>
+
+<svelte:head>
+	<title>Login / Register</title>
+</svelte:head>
+
+<Container class="my-5">
+	<Row>
+		<Col sm="12" md={{ size: 10, offset: 1 }} lg={{ size: 6, offset: 3 }}>
+			UPDATE
+			<LoginRegisterManager onLoginSuccess={() => goto('/identity-manager')} />
+		</Col>
+	</Row>
+</Container>
