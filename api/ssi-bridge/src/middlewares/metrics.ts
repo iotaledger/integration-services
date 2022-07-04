476000d8e5eb66c0fa855195cc0cd5cb6840bbb9
Michele Mastrogiovanni <michele.mastrogiovanni@gmail.com>
2022-07-04 12:13:40 +0200

    Added some buckets to histogram

--- api/ssi-bridge/src/middlewares/metrics.ts
+++ api/ssi-bridge/src/middlewares/metrics.ts
@@ -14,7 +14,7 @@ const httpRequestDurationSeconds = new Histogram({
 	name: 'http_request_duration_seconds',
 	help: 'Duration of HTTP requests in seconds',
 	labelNames: ['method', 'path', 'code'],
-	buckets: [0.1, 2, 5, 10, 20, 40, 60]
+	buckets: [0.1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20, 40, 60]
 });
 
 // Create a gauge metric
