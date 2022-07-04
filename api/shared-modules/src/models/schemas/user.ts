39b5555adbdbf38d36a34b14786a9c365c9cfcab
Dominic Zettl <dominic.zettl@iota.org>
2022-07-04 15:50:05 +0200

    remove publickey from user data

--- api/shared-modules/src/models/schemas/user.ts
+++ api/shared-modules/src/models/schemas/user.ts
@@ -23,7 +23,6 @@ export const IdentityWithoutIdAndCredentialFields = {
 
 export const IdentitySchema = Type.Object({
 	id: Type.String({ minLength: 50, maxLength: 53 }), // did
-	publicKey: Type.String({ minLength: 10 }),
 	creator: Type.Optional(Type.String({ minLength: 50, maxLength: 53 })),
 	...IdentityWithoutIdFields
 });
