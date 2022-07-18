2b60adb1d2ac0551bfae7c362a62e91ac48addfc
Dominic Zettl <dominic.zettl@iota.org>
2022-07-18 17:21:52 +0200

    fix authentication method

--- api/ssi-bridge/src/services/authentication-service.ts
+++ api/ssi-bridge/src/services/authentication-service.ts
@@ -1,5 +1,5 @@
 import { UserService } from './user-service';
-import { createNonce, verifySignedNonce, User, UserRoles } from '@iota/is-shared-modules';
+import { createNonce, verifySignedNonce, User, UserRoles, getHexEncodedKey } from '@iota/is-shared-modules';
 import * as AuthDb from '../database/auth';
 import jwt from 'jsonwebtoken';
 import { AuthenticationServiceConfig } from '../models/config/services';
@@ -48,7 +48,8 @@ export class AuthenticationService {
 		}
 
 		const { nonce } = await AuthDb.getNonce(id);
-		const verified = await verifySignedNonce(publicKey, nonce, signedNonce);
+		const encodedPublicKey = getHexEncodedKey(publicKey);
+		const verified = await verifySignedNonce(encodedPublicKey, nonce, signedNonce);
 
 		if (!verified) {
 			throw new Error('signed nonce is not valid!');
