534d36201f367af6124cca395a9ef21ebb927732
Tim Sigl <tim.sigl@hotmail.de>
2022-07-22 07:25:25 +0200

    seperated browser and node imports

---
+++ api/shared-modules/src/index-node.ts
@@ -0,0 +1,5 @@
+export * from './models';
+export * from './services/mongodb-service';
+export * from './utils/encryption';
+export * from './utils/logger';
+export * from './utils/text';
--- api/shared-modules/src/index.ts
+++
@@ -1,7 +0,0 @@
-export { MongoDbService } from './services/mongodb-service';
-export { createNonce, decrypt, encrypt, getHexEncodedKey, randomSecretKey, signNonce, verifySignedNonce } from './utils/encryption';
-export { Logger, ILogger } from './utils/logger';
-export { fromBytes, getDateFromString, getDateStringFromDate, toBytes } from './utils/text';
-export * from './models/schemas';
-export * from './models/schemas/request-response-body';
-export * from './models/types';
