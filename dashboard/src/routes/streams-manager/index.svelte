4f46b67a4e7b532a1a2db7bd7c64186796b5d04f
Juri Bogatyrjow <juri.bogatyrjow@gmail.com>
2022-08-18 12:44:55 +0200

    dashboard update

---
+++ dashboard/src/routes/streams-manager/index.svelte
@@ -0,0 +1,20 @@
+<script context="module" lang="ts">
+    export const prerender = true
+</script>
+
+<script lang="ts">
+    import { StreamsManager } from '@iota/is-ui-components'
+    import { Col, Container, Row } from 'sveltestrap'
+</script>
+
+<svelte:head>
+    <title>Streams Manager</title>
+</svelte:head>
+
+<Container class="my-5">
+    <Row>
+        <Col sm="12" md={{ size: 10, offset: 1 }}>
+            <StreamsManager />
+        </Col>
+    </Row>
+</Container>
